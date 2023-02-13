6da0b17cd0574abbcd72752eeed0bc2f5aea98dc
AyodejiOyebanji <oyeayodejioyebanji2003@gmail.com>
2023-02-13 03:03:41 +0100

    third commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // url:"https://fintech-server-side-6vlg.vercel.app/users"
-  url:"https://fintechappnew-7wbo2v844-ayodejioyebanji.vercel.app/users"
+  url:"https://fintech-server-side-6vlg.vercel.app/users"
+  // url:"https://fintechappnew-7wbo2v844-ayodejioyebanji.vercel.app/users"
 
 };
